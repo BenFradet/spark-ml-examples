cc80da2681d5efc4924c58b8971ad403cf82a3b2
BenFradet <benjamin.fradet@gmail.com>
2016-11-03 18:11:19 +0100

    finished the code for chapter 2

--- chapter2/src/main/scala/io/github/benfradet/spark/ml/in/action/Titanic.scala
+++ chapter2/src/main/scala/io/github/benfradet/spark/ml/in/action/Titanic.scala
@@ -1,6 +1,11 @@
 package io.github.benfradet.spark.ml.in.action
 
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.feature.{VectorAssembler, StringIndexer}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.avg
+import org.apache.spark.sql.types.{IntegerType, DoubleType}
 
 /** This is the code related to chapter 2. */
 object Titanic {
@@ -12,8 +17,9 @@ object Titanic {
 
     val spark = SparkSession
       .builder()
-      .appName("Data preparation for chapter 7")
+      .appName("Chapter 2")
       .getOrCreate()
+    import spark.implicits._
 
     val trainFilePath = args(0)
     val titanicTrain = spark
@@ -22,8 +28,38 @@ object Titanic {
       .option("header", "true")
       .option("inferSchema", "true")
       .load(trainFilePath)
+      .withColumn("Survived", $"Survived".cast(DoubleType))
     titanicTrain.printSchema()
     titanicTrain.show(5, truncate = false)
+    println(titanicTrain.where($"Age".isNull).count())
+    println(titanicTrain.where($"Fare".isNull).count())
+
+    val avgAge = titanicTrain.select(avg($"Age")).first().getDouble(0)
+    val avgFare = titanicTrain.select(avg($"Fare")).first().getDouble(0)
+    val imputedMap = Map("Age" -> avgAge, "Fare" -> avgFare)
+    val imputedTitanicTrain = titanicTrain.na.fill(imputedMap)
+
+    val categoricalCols = Seq("Pclass", "Sex", "Embarked")
+    val indexers = categoricalCols.map { colName =>
+      new StringIndexer()
+        .setInputCol(colName)
+        .setOutputCol(colName + "Indexed")
+    }
+
+    val numericalCols = Seq("Age", "SibSp", "Parch", "Fare")
+    val featuresCol = "features"
+    val assembler = new VectorAssembler()
+      .setInputCols(Array(numericalCols ++ categoricalCols.map(_ + "Indexed"): _*))
+      .setOutputCol(featuresCol)
+
+    val labelCol = "Survived"
+    val decisionTree = new DecisionTreeClassifier()
+      .setLabelCol(labelCol)
+      .setFeaturesCol(featuresCol)
+
+    val pipeline = new Pipeline().setStages(Array(indexers :+ assembler :+ decisionTree: _*))
+
+    val model = pipeline.fit(imputedTitanicTrain)
 
     val testFilePath = args(1)
     val titanicTest = spark
@@ -34,6 +70,21 @@ object Titanic {
       .load(testFilePath)
     titanicTest.printSchema()
     titanicTest.show(5, truncate = false)
+    println(titanicTest.where($"Age".isNull).count())
+    println(titanicTest.where($"Fare".isNull).count())
+
+    val imputedTitanicTest = titanicTest.na.fill(imputedMap)
+
+    val predictions = model.transform(imputedTitanicTest)
+
+    val outputPath = args(2)
+    predictions
+      .select($"PassengerId", $"prediction".cast(IntegerType).alias("Survived"))
+      .coalesce(1)
+      .write
+      .format("csv")
+      .option("header", "true")
+      .save(outputPath)
 
     spark.stop()
   }
