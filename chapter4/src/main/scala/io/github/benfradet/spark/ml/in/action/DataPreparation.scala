8bba45c1d8bf4f7503182cfd8cb8ed87d989f430
Benjamin Fradet <benjamin.fradet@gmail.com>
2017-02-11 19:27:35 +0000

    [c4] Split issue projection, fixes #22

--- chapter4/src/main/scala/io/github/benfradet/spark/ml/in/action/DataPreparation.scala
+++ chapter4/src/main/scala/io/github/benfradet/spark/ml/in/action/DataPreparation.scala
@@ -21,33 +21,41 @@ object DataPreparation {
     val inputPath = args(0)
     val events = spark.read.json(inputPath)
     //events.printSchema()
-    //events.show(5, truncate = false)
+    //events.show(5)
 
-    val issueEvents = events
-      .filter($"type" === "IssuesEvent" &&
-        !$"payload.issue.title".isNull && !$"payload.issue.body".isNull)
+    val issueEvents = events.filter($"type" === "IssuesEvent")
     val projectedIssues = issueEvents.select(
-      concat($"payload.issue.title", lit(" "), $"payload.issue.body").as("text"),
+      $"payload.issue.title",
+      $"payload.issue.body",
       $"payload.issue.labels"
+    )
+    // Replace null bodies by empty strings
+    val noNullBodyIssues = projectedIssues.na.fill("")
+    val concatIssues = noNullBodyIssues.select(
+      concat($"title", lit(" "), $"body").as("text"),
+      $"labels"
     )
     // No label default
     //projectedIssues.printSchema()
-    //projectedIssues.show(5, truncate = false)
+    //projectedIssues.show(5)
+
+    val ghIssues = concatIssues.as[GHIssue]
+    //ghIssues.show(5)
 
     // Consider only the bug, enhancement and question default labels because of
     //import org.apache.spark.sql.functions.desc
-    //projectedIssues
-    //  .as[GHIssue]
+    //ghIssues
     //  .flatMap(_.labels.map(_.name))
     //  .groupBy("value")
     //  .count()
     //  .orderBy(desc("count"))
     //  .show(5)
     val possibleLabels = Set("enhancement", "bug", "question")
-    val issues = projectedIssues
-      .as[GHIssue]
+    val issues = ghIssues
       .flatMap { i =>
-        val labels = i.labels.map(_.name).filter(possibleLabels.contains)
+        val labels = i.labels
+          .map(_.name)
+          .filter(possibleLabels.contains)
         labels.map(n => Issue(i.text.replaceAll("[\n\r]|```.*```", " "), n))
       }
     //issues.groupBy("label").count().show()
